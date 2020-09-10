d564405ad5869a11d294443ebb36d6fb700e1623
rpsychologist <1629860+rpsychologist@users.noreply.github.com>
2020-09-10 17:46:44 +0000

    Show member count

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -2,6 +2,6 @@ module.exports = member => {
     let guild = member.guild; 
     let memberId= member.user.id; 
     if(guild.systemChannel) { 
-        guild.systemChannel.send(`Welcome to the **Git Gud Science** discord, <@${memberId}>! Please read the instructions in <#714057330219548693> and join the conversations!`);
+        guild.systemChannel.send(`Welcome to the **Git Gud Science** discord, <@${memberId}> (Member #${guild.memberCount})! Please read the instructions in <#714057330219548693> and join the conversations!`);
     }
   };
